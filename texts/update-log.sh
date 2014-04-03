8ce881329f9a257caf5dfd8dfe0d12d61ec4297a
Francis M. Tyers <ftyers@prompsit.com>
2014-04-03 11:53:37 +0000

    update log to count fully parsed sentences

--- texts/update-log.sh
+++ texts/update-log.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 DATE=`date`
 TOKENS=`cat $1 | grep '\/' | wc -l`
 UNKNOWN=`cat $1 | grep '\*' | wc -l`;
@@ -6,4 +8,6 @@ FUNCTAGS=`cat $1 | grep '@' | wc -l`;
 FUNCCOV=`echo \($FUNCTAGS/$TOKENS\)\*100 | bc -l | sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
 COVERAGE=`echo \(\($TOKENS-$UNKNOWN\)/$TOKENS\)\*100 | bc -l | sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
 AMBIG=`echo $ANALYSES/$TOKENS | bc -l |  sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
-echo -e "$DATE\t$1\t$TOKENS\t$UNKNOWN\t$COVERAGE\t$ANALYSES\t$AMBIG\t$FUNCTAGS\t$FUNCCOV" 
+TOTALSENT=`cat murmansk.ana.txt | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | wc -l`;
+FUNCSENT=`cat murmansk.ana.txt | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | grep -v '<[a-z]\+>#' | wc -l`
+echo -e "$DATE\t$1\t$TOKENS\t$UNKNOWN\t$COVERAGE\t$ANALYSES\t$AMBIG\t$FUNCTAGS\t$FUNCCOV\t$TOTALSENT\t$FUNCSENT" 
