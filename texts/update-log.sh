fb473492f04181161c7a47b39af16d5cce82b171
Francis M. Tyers <ftyers@prompsit.com>
2014-04-07 19:48:02 +0000

    add rwanda text

--- texts/update-log.sh
+++ texts/update-log.sh
@@ -8,6 +8,6 @@ FUNCTAGS=`cat $1 | grep '@' | wc -l`;
 FUNCCOV=`echo \($FUNCTAGS/$TOKENS\)\*100 | bc -l | sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
 COVERAGE=`echo \(\($TOKENS-$UNKNOWN\)/$TOKENS\)\*100 | bc -l | sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
 AMBIG=`echo $ANALYSES/$TOKENS | bc -l |  sed 's/\(\.[0-9][0-9]\)\(.*\)/\1/g'`;
-TOTALSENT=`cat murmansk.ana.txt | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | wc -l`;
-FUNCSENT=`cat murmansk.ana.txt | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | grep -v '<[a-z]\+>#' | wc -l`
+TOTALSENT=`cat $1 | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | wc -l`;
+FUNCSENT=`cat $1 | tr '\n' ' ' | sed 's/>\\$/>#/g' | sed 's/¶/\n/g' | grep -v '^$' | grep -v '<[a-z]\+>#' | wc -l`
 echo -e "$DATE\t$1\t$TOKENS\t$UNKNOWN\t$COVERAGE\t$ANALYSES\t$AMBIG\t$FUNCTAGS\t$FUNCCOV\t$TOTALSENT\t$FUNCSENT" 
