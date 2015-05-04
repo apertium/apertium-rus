98dac7866654214ee2bf599778ebb06224994802
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-05-04 14:47:59 +0000

    beginnings of Cg mode

--- scripts/corpconv/rnc2cg.py
+++ scripts/corpconv/rnc2cg.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse, re
-import xml.etree.ElementTree as etree
+import xml.etree.ElementTree as ET
 
 stressMark = '`'
 
@@ -9,7 +9,7 @@ def parseRnc(fn):
 	global stressMark
 	with open(fn, 'r', encoding="windows-1251") as corpusFile:
 		content = corpusFile.read()
-	corpusTree = etree.fromstring(content)
+	corpusTree = ET.fromstring(content)
 
 	return corpusTree
 
@@ -24,6 +24,17 @@ def printRnc(corpus, stress=False):
 	#print(words)
 	print(' '.join(words))
 
+def printRncAsCg(corpus):
+	for word in corpus.findall('.//se/w'):
+		wd = ''.join(word.itertext())
+		anas = word.findall('ana')
+		if len(anas)==1:
+			ana = anas[0].attrib
+		else: print("WARNING: more than one analysis!!!  Keeping only first for the moment")
+		#for ana in word.findall('ana'):
+		#	print(ana.attrib)
+		print(wd, ana)
+
 
 
 if __name__ == '__main__':
@@ -31,6 +42,7 @@ if __name__ == '__main__':
 	parser.add_argument('corpus', help="uri to a corpus file")
 	parser.add_argument('-s', '--stress', help="preserve stress marks", action='store_true', default=False)
 	parser.add_argument('-c', '--clean', help="print clean output", action='store_true', default=False)
+	parser.add_argument('-g', '--cg', help="print raw corpus in CG format", action='store_true', default=False)
 
 	args = parser.parse_args()
 
@@ -38,4 +50,5 @@ if __name__ == '__main__':
 
 	if(args.clean):
 		printRnc(corpus, stress=args.stress)
-
+	elif(args.cg):
+		printRncAsCg(corpus)
