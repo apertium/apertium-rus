20d33b5c7598ef236b00c5a29d19540851dcc7ee
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-05-04 14:13:32 +0000

    stress mark preservation

--- scripts/corpconv/rnc2cg.py
+++ scripts/corpconv/rnc2cg.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-import argparse
+import argparse, re
 import xml.etree.ElementTree as etree
 
-def parseRnc(fn):
+stressMark = '`'
+
+def parseRnc(fn, stress=False):
+	global stressMark
 	with open(fn, 'r', encoding="windows-1251") as corpusFile:
 		content = corpusFile.read()
 	corpusTree = etree.fromstring(content)
@@ -11,6 +14,8 @@ def parseRnc(fn):
 	words = []
 	for word in corpusTree.itertext():
 		if word.strip()!='':
+			if not stress:
+				word = re.sub(stressMark, "", word)
 			words.append(word.strip())
 
 	#print(words)
@@ -20,6 +25,8 @@ def parseRnc(fn):
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='rnc 2 cg mangler')
 	parser.add_argument('corpus', help="uri to a corpus file")
+	parser.add_argument('-s', '--stress', help="preserve stress marks", action='store_true', default=False)
+
 	args = parser.parse_args()
 
-	parseRnc(args.corpus)
+	parseRnc(args.corpus, stress=args.stress)
