08df0aa148ee1dec81e7ad099a0dad0b8a7fa408
Jonathan North Washington <jonathan.n.washington@gmail.com>
2015-05-04 14:21:41 +0000

    output clean text as an option

--- scripts/corpconv/rnc2cg.py
+++ scripts/corpconv/rnc2cg.py
@@ -5,14 +5,17 @@ import xml.etree.ElementTree as etree
 
 stressMark = '`'
 
-def parseRnc(fn, stress=False):
+def parseRnc(fn):
 	global stressMark
 	with open(fn, 'r', encoding="windows-1251") as corpusFile:
 		content = corpusFile.read()
 	corpusTree = etree.fromstring(content)
 
+	return corpusTree
+
+def printRnc(corpus, stress=False):
 	words = []
-	for word in corpusTree.itertext():
+	for word in corpus.itertext():
 		if word.strip()!='':
 			if not stress:
 				word = re.sub(stressMark, "", word)
@@ -22,11 +25,17 @@ def parseRnc(fn, stress=False):
 	print(' '.join(words))
 
 
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='rnc 2 cg mangler')
 	parser.add_argument('corpus', help="uri to a corpus file")
 	parser.add_argument('-s', '--stress', help="preserve stress marks", action='store_true', default=False)
+	parser.add_argument('-c', '--clean', help="print clean output", action='store_true', default=False)
 
 	args = parser.parse_args()
 
-	parseRnc(args.corpus, stress=args.stress)
+	corpus = parseRnc(args.corpus)
+
+	if(args.clean):
+		printRnc(corpus, stress=args.stress)
+
